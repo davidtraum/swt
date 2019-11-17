188f3e7eec51e1d85442c55fd2230f831bd924fa
Thallwin <thallinho91.mk@gmail.com>
2019-11-17 21:08:59 +0100

    Kurven

--- work_in_progress/RailClass.py
+++ work_in_progress/RailClass.py
@@ -1,30 +1,62 @@
 from GameLogicObject import GameLogicObject
+from enum import Enum
 
 class RailLogic(GameLogicObject):
 
+    def __init__(self, px_Pos, py_Pos):
+        self.x_Pos = px_Pos;
+        self.y_Pos = py_Pos;        
+        self.Direction = 0; #0 ist horizontal #1 ist vertikal
+
     @staticmethod
     def _checkRails(self, x_Pos, y_Pos, karte):
-        railExistsUp = 0; #4-dim Array für 4 Seiten eines Quadranten
+        railExistsUp = 0; 
         railExistsDown = 0;
         railExistsLeft = 0;
         railExistsRight = 0;
 
-        if (karte[x_Pos+1][y_Pos] == "rail"):
+        if (karte[x_Pos+1][y_Pos] == RailLogic):
             #hier check inhalt
             railExistsRight = 1;
 
-        if (karte[x_Pos][y_Pos+1] == "rail"):
+        if (karte[x_Pos][y_Pos+1] == RailLogic):
             #hier check inhalt
             railExistsDown = 1;
 
-        if (karte[x_Pos-1][y_Pos] == "rail"):
+        if (karte[x_Pos-1][y_Pos] == RailLogic):
             #hier check inhalt
             railExistsLeft = 1;
 
-        if (karte[x_Pos][y_Pos-1] == "rail"):
+        if (karte[x_Pos][y_Pos-1] == RailLogic):
             #hier check inhalt
-            railExistsUp = 1;
+            railExistsUp = 1;                
+                
 
         return railExistsUp, railExistsDown, railExistsRight, railExistsLeft;
 
         
+
+
+class CurveDirection(Enum):
+    topRight = 1
+    topLeft = 2
+    downRight = 3
+    downLeft = 4
+    rightUp = 5
+    rightDown = 6
+    leftUp = 7
+    leftDown = 8
+    unknown = 9
+
+    
+
+class CurveLogic(RailLogic):
+
+    def __init__(self, px_Pos, py_Pos):
+        self.x_Pos = px_Pos;
+        self.y_Pos = py_Pos;        
+        self.curveDirection = CurveDirection.unknown
+
+        
+        
+
--- work_in_progress/World.py
+++ work_in_progress/World.py
@@ -1,4 +1,4 @@
-from RailClass import RailLogic
+from RailClass import *
 
 class Grass:
     def __init__(self, px_Pos, py_Pos):
@@ -15,11 +15,22 @@ class World:
     def changeMaptile(self, px, py, newMaptile):
         self.map[px][py] = newMaptile;
 
-    def railPlaced(self, px, py):
+    def checkRailSurroundings(self, px, py):
         self.railExistsUp, self.railExistsDown, self.railExistsRight, self.railExistsLeft = RailLogic._checkRails(self, px, py, self.map);
 
+    def setRail(self, px, py, rail):
+        self.railPlaced(px, py); 
+        if (railExistsUp + railExistsDown + railExistsRight + railExistsLeft == 1):
+            #wenn nur eine andere Schiene braucht es maximal eine Kurve/max. 1 Maptile muss verändert werden
+            if (railExistsRight == 1 and self.direction == 0):  #upRight
+                curve = CurveLogic
+                CurveLogic.curvedirection = CurveDirection.UpRight
+                self.map[px-1][py] = curve
+
+
 karte = World();
-karte.railPlaced(4,7);
+karte.map[4][8] = RailLogic;
+karte.checkRailSurroundings(4,7);
 print(karte.railExistsUp);
 print(karte.railExistsDown);
 print(karte.railExistsRight);
